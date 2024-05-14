92db6bf02d1a6f6a56a2291d0fd68422122723e0
Hadrien G. <knights_of_ni@gmx.com>
2024-05-14 09:05:19 +0200

    Benchmark polish

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -14,28 +14,71 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         }
     }
 
-    // Query the bitmap at the same hidden location
+    // Query the bitmap at the same hidden locations
     //
     // In this benchmark, the optimizer knows that we're accessing the same
-    // bitmap in the loop but it doesn't know at which index (besides that this
-    // index is resident in a CPU register at the start of the computation).
+    // bitmap in the loop but it doesn't know at which indices (besides the fact
+    // that each index will be resident in a general-purpose CPU register at the
+    // start of the computation).
     //
     // This models a real-world complex indexing workload, where the computation
     // of the index at which the bitmap is probed is too complex for the
     // optimizer to make sense of.
     {
+        // Loop unrolling factor used to amortize benchmark harness and
+        // optimization barrier overheads. Should not be greater than 4 because
+        // x86_64 has 16 registers and we need UNROLL_FACTOR of them for inputs
+        // and UNROLL_FACTOR for outputs.
+        const UNROLL_FACTOR: usize = 4;
         let mut g = c.benchmark_group("hidden_constant");
-        g.throughput(Throughput::Elements(1));
+        g.throughput(Throughput::Elements(UNROLL_FACTOR as u64));
         let bitmap = hidden_bitmap();
-        let index = pessimize::hide(123);
+        let indices = [123, 456, 789, 1011];
+        assert_eq!(indices.len(), UNROLL_FACTOR);
         g.bench_function("bit_naive", |b| {
-            b.iter(|| pessimize::consume(bit_naive(bitmap, pessimize::hide(index))))
+            b.iter(|| {
+                let [i1, i2, i3, i4] = indices;
+                let [o1, o2, o3, o4] = [
+                    bit_naive(bitmap, pessimize::hide(i1)),
+                    bit_naive(bitmap, pessimize::hide(i2)),
+                    bit_naive(bitmap, pessimize::hide(i3)),
+                    bit_naive(bitmap, pessimize::hide(i4)),
+                ];
+                pessimize::consume(o1);
+                pessimize::consume(o2);
+                pessimize::consume(o3);
+                pessimize::consume(o4);
+            })
         });
         g.bench_function("bit_const_table", |b| {
-            b.iter(|| pessimize::consume(bit_const_table(bitmap, pessimize::hide(index))))
+            b.iter(|| {
+                let [i1, i2, i3, i4] = indices;
+                let [o1, o2, o3, o4] = [
+                    bit_const_table(bitmap, pessimize::hide(i1)),
+                    bit_const_table(bitmap, pessimize::hide(i2)),
+                    bit_const_table(bitmap, pessimize::hide(i3)),
+                    bit_const_table(bitmap, pessimize::hide(i4)),
+                ];
+                pessimize::consume(o1);
+                pessimize::consume(o2);
+                pessimize::consume(o3);
+                pessimize::consume(o4);
+            })
         });
         g.bench_function("bit_static_table", |b| {
-            b.iter(|| pessimize::consume(bit_static_table(bitmap, pessimize::hide(index))))
+            b.iter(|| {
+                let [i1, i2, i3, i4] = indices;
+                let [o1, o2, o3, o4] = [
+                    bit_static_table(bitmap, pessimize::hide(i1)),
+                    bit_static_table(bitmap, pessimize::hide(i2)),
+                    bit_static_table(bitmap, pessimize::hide(i3)),
+                    bit_static_table(bitmap, pessimize::hide(i4)),
+                ];
+                pessimize::consume(o1);
+                pessimize::consume(o2);
+                pessimize::consume(o3);
+                pessimize::consume(o4);
+            })
         });
     }
 
@@ -46,28 +89,91 @@ pub fn criterion_benchmark(c: &mut Criterion) {
     // that it's the same bitmap on each iteration of the outer benchmark loop.
     {
         let mut g = c.benchmark_group("linear");
-        g.throughput(Throughput::Elements(hidden_bitmap().len() as u64));
+        g.throughput(Throughput::Elements((hidden_bitmap().len() * 8) as u64));
         g.bench_function("bit_naive", |b| {
             b.iter(|| {
                 let bitmap = hidden_bitmap();
-                for index in 0..bitmap.len() {
-                    pessimize::consume(bit_naive(bitmap, index));
+                for byte in 0..bitmap.len() {
+                    let first_bit = byte * 8;
+                    // Here we can use 8-way loop unrolling because the input
+                    // pattern is simple and not forced to stay resident in
+                    // registers.
+                    let [o1, o2, o3, o4, o5, o6, o7, o8] = [
+                        bit_naive(bitmap, first_bit),
+                        bit_naive(bitmap, first_bit + 1),
+                        bit_naive(bitmap, first_bit + 2),
+                        bit_naive(bitmap, first_bit + 3),
+                        bit_naive(bitmap, first_bit + 4),
+                        bit_naive(bitmap, first_bit + 5),
+                        bit_naive(bitmap, first_bit + 6),
+                        bit_naive(bitmap, first_bit + 7),
+                    ];
+                    pessimize::consume(o1);
+                    pessimize::consume(o2);
+                    pessimize::consume(o3);
+                    pessimize::consume(o4);
+                    pessimize::consume(o5);
+                    pessimize::consume(o6);
+                    pessimize::consume(o7);
+                    pessimize::consume(o8);
                 }
             })
         });
         g.bench_function("bit_const_table", |b| {
             b.iter(|| {
                 let bitmap = hidden_bitmap();
-                for index in 0..bitmap.len() {
-                    pessimize::consume(bit_const_table(bitmap, index));
+                for byte in 0..bitmap.len() {
+                    let first_bit = byte * 8;
+                    // Here we can use 8-way loop unrolling because the input
+                    // pattern is simple and not forced to stay resident in
+                    // registers.
+                    let [o1, o2, o3, o4, o5, o6, o7, o8] = [
+                        bit_const_table(bitmap, first_bit),
+                        bit_const_table(bitmap, first_bit + 1),
+                        bit_const_table(bitmap, first_bit + 2),
+                        bit_const_table(bitmap, first_bit + 3),
+                        bit_const_table(bitmap, first_bit + 4),
+                        bit_const_table(bitmap, first_bit + 5),
+                        bit_const_table(bitmap, first_bit + 6),
+                        bit_const_table(bitmap, first_bit + 7),
+                    ];
+                    pessimize::consume(o1);
+                    pessimize::consume(o2);
+                    pessimize::consume(o3);
+                    pessimize::consume(o4);
+                    pessimize::consume(o5);
+                    pessimize::consume(o6);
+                    pessimize::consume(o7);
+                    pessimize::consume(o8);
                 }
             })
         });
         g.bench_function("bit_static_table", |b| {
             b.iter(|| {
                 let bitmap = hidden_bitmap();
-                for index in 0..bitmap.len() {
-                    pessimize::consume(bit_static_table(bitmap, index));
+                for byte in 0..bitmap.len() {
+                    let first_bit = byte * 8;
+                    // Here we can use 8-way loop unrolling because the input
+                    // pattern is simple and not forced to stay resident in
+                    // registers.
+                    let [o1, o2, o3, o4, o5, o6, o7, o8] = [
+                        bit_static_table(bitmap, first_bit),
+                        bit_static_table(bitmap, first_bit + 1),
+                        bit_static_table(bitmap, first_bit + 2),
+                        bit_static_table(bitmap, first_bit + 3),
+                        bit_static_table(bitmap, first_bit + 4),
+                        bit_static_table(bitmap, first_bit + 5),
+                        bit_static_table(bitmap, first_bit + 6),
+                        bit_static_table(bitmap, first_bit + 7),
+                    ];
+                    pessimize::consume(o1);
+                    pessimize::consume(o2);
+                    pessimize::consume(o3);
+                    pessimize::consume(o4);
+                    pessimize::consume(o5);
+                    pessimize::consume(o6);
+                    pessimize::consume(o7);
+                    pessimize::consume(o8);
                 }
             })
         });
